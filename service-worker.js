47d0bdb4bc4ecb73776d06a51184a33f67f5f95e
SLON TRAKTOR <148796306+Deuschland@users.noreply.github.com>
2025-09-29 16:11:17 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1.0.8';
+const CACHE_VERSION = 'v1.0.9';
 const CACHE_NAME = `tarif-cache-${CACHE_VERSION}`;
 const FILES_TO_CACHE = [
   './index.html',
@@ -72,5 +72,6 @@ self.addEventListener('fetch', event => {
     )
   );
 });
+
 
 
